fc694c505a2b21d8a06f5ffa69cbbb0f887a41da
richard.qiu <richardyuhua@outlook.com>
2018-03-29 16:29:10 +0800

    AntiOutSiteLinkAttribute

---
+++ WindowsServer.NetFramework/MvcCommon/Web/Filters/AntiOutSiteLinkAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace WindowsServer.Web.Filters
+{
+    public class AntiOutSiteLinkAttribute : ActionFilterAttribute
+    {
+        public enum FileType
+        {
+            File = 1,
+            Image
+        }
+
+        public FileType FileType { get; set; }
+
+        public AntiOutSiteLinkAttribute(FileType fileType) => this.FileType = fileType;
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+    }
+}
