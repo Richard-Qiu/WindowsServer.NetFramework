cc10673ef3b9dfc9bd7dbc5c5036e6ba62152784
RichardQiu <Richardyuhua@outlook.com>
2018-07-09 10:30:25 +0800

    添加Utility, 包含HttpWebRequestClient

---
+++ WindowsServer.NetFramework/ServerCommon/Utlitity/TimeUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServer.Utlitity
+{
+    public class TimeUtility
+    {
+        public static int ConvertDateTimeInt(System.DateTime time)
+        {
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            return (int)(time - startTime).TotalSeconds;
+        }
+
+    }
+}
